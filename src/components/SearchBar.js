78d549171364f3e44867f4949fcd822c43f36a5f
Simone Taeggi <startae14@gmail.com>
2021-04-27 16:02:23 +0200

    resolve context problems, this undefined, defining the methods of a class as arrow functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   state = { term: '' };
+
+  onFormSubmit = (event) => {
+    // prevent default refresh
+    event.preventDefault();
+
+    console.log(this.state.term);
+  }
   
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input type='text' value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
