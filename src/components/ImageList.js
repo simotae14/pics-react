e79ce6ac60d70ad930bfd6e60b64cccb96088c0f
Simone <startae14@gmail.com>
2021-04-27 23:47:41 +0200

    11: rendering the images using the map method

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  console.log(props.images);
+  const images = props.images.map(image => <img alt={image.alt_description} src={image.urls.regular} />)
   return (
     <div>
-      ImageList
+      {images}
     </div>
   );
 };
