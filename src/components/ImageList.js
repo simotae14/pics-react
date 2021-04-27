a6aa7250c957c2687fceae15e10a11cbc96e3f07
Simone <startae14@gmail.com>
2021-04-28 00:12:57 +0200

    13: define a grid layout for the ImageList content

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = (props) => {
   const images = props.images.map(({ id, alt_description, urls  }) => <img key={id} alt={alt_description} src={urls.regular} />)
   return (
-    <div>
+    <div className="image-list">
       {images}
     </div>
   );
