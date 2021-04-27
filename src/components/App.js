43ecf49c7f3995ea1471d99818dd7dad39d452a9
Simone <startae14@gmail.com>
2021-04-27 22:58:44 +0200

    08: transform the callback in a arrow function to fix the this problem

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = {
+    images: []
+  };
+  onSearchSubmit = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
         query: term
@@ -12,12 +15,15 @@ class App extends React.Component {
         Authorization: 'Client-ID lyaLU6WprjdmKfXfRa19xjDztOJcX99eDUFAAOrxkVw'
       }
     });
-    console.log(response.data.results);
+    this.setState({
+      images: response.data.results
+    });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: { this.state.images.length }
       </div>
     );
   }
