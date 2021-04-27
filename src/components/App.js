e115144986d2c3a310d1a23cb92e0e3eae045ee2
Simone <startae14@gmail.com>
2021-04-27 22:47:17 +0200

    07: handle the response of the API with async/await

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
         query: term
       },
       headers: {
         Authorization: 'Client-ID lyaLU6WprjdmKfXfRa19xjDztOJcX99eDUFAAOrxkVw'
       }
-    })
-    .then((response) => {
-      console.log(response.data.results);
     });
+    console.log(response.data.results);
   }
   render() {
     return (
